24e4fc33f58695f7cff5e713434df172f15e0298
kimlydeviceacct-afk <kimlydeviceacct@gmail.com>
2026-09-19 19:52:51 +0700

    Correct wording

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function start() {
                 const visits = await client.get('visits');
 
                 // Return current number
-                res.send('Number of visitor is ' + visits);
+                res.send('Number of visitor = ' + visits);
 
                 // Increment for next visit
                 await client.set('visits', parseInt(visits) + 1);
